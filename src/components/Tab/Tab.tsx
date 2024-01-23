0c79793b4325ebf907876f2e477bbcecb98197d1
TungPT12 <mylifeundermycontrol@gmail.com>
2024-01-23 18:27:16 +0700

    tab

--- src/components/Tab/Tab.tsx
+++ src/components/Tab/Tab.tsx
@@ -1,52 +1,90 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useMemo } from "react";
 import styles from "./Tab.module.scss";
 const Tabs = () => {
-  const [activeTab, setActiveTab] = useState(0);
+  const [activeTab, setActiveTab] = useState({
+    isShowDescription: true,
+    isShowInformation: false,
+    isShowReview: false,
+  });
 
-  const handleTabClick = (index: any) => {
-    setActiveTab(index);
+  const renderTabs = () => {
+    if (activeTab.isShowDescription) {
+      return (
+        <div className={`${styles.tabContent}`}>
+          <h3>Content for Tab 1</h3>
+          <span>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+            commodo ligula eget dolor. Aenean massa. Cum sociis natoque
+            penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+          </span>
+        </div>
+      );
+    } else if (activeTab.isShowInformation) {
+      return (
+        <div className={`${styles.tabContent}`}>
+          <h3>Content for Tab 2</h3>
+          <p>This is the content for Tab 2.</p>
+        </div>
+      );
+    } else {
+      return (
+        <div className={`${styles.tabContent}`}>
+          <h3>Content for Tab 3</h3>
+          <p>Dimensions:20 × 10 × 30 cm</p>
+          <p>Dimensions:20 × 10 × 30 cm</p>
+        </div>
+      );
+    }
   };
 
   return (
     <div className={styles.tabContainer}>
       <div className={styles.tabCtroll}>
-        <div className={styles.tab} onClick={() => handleTabClick(0)}>
+        <div
+          className={`${styles["tab"]} ${
+            activeTab.isShowDescription ? styles["active"] : ""
+          } `}
+          onClick={() => {
+            setActiveTab({
+              isShowInformation: false,
+              isShowReview: false,
+              isShowDescription: true,
+            });
+          }}
+        >
           Description
         </div>
-        <div className={styles.tab} onClick={() => handleTabClick(1)}>
+        <div
+          className={`${styles["tab"]} ${
+            activeTab.isShowInformation ? styles["active"] : ""
+          }`}
+          onClick={() => {
+            setActiveTab({
+              isShowDescription: false,
+              isShowInformation: true,
+              isShowReview: false,
+            });
+          }}
+        >
           INFORMATION
         </div>
-        <div className={styles.tab} onClick={() => handleTabClick(2)}>
+        <div
+          className={`${styles["tab"]} ${
+            activeTab.isShowReview ? styles["active"] : ""
+          }`}
+          onClick={() => {
+            setActiveTab({
+              isShowDescription: false,
+              isShowInformation: false,
+              isShowReview: true,
+            });
+          }}
+        >
           Review (1)
         </div>
       </div>
-
-      <div
-        className={`${styles.tabContent} ${activeTab === 0 ? styles.show : ""}`}
-      >
-        <h3>Content for Tab 1</h3>
-        <span>
-          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
-          commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus
-          et magnis dis parturient montes, nascetur ridiculus mus.
-        </span>
-      </div>
-
-      <div
-        className={`${styles.tabContent} ${activeTab === 1 ? styles.show : ""}`}
-      >
-        <h3>Content for Tab 2</h3>
-        <p>This is the content for Tab 2.</p>
-      </div>
-
-      <div
-        className={`${styles.tabContent} ${activeTab === 2 ? styles.show : ""}`}
-      >
-        <h3>Content for Tab 3</h3>
-        <p>Dimensions:20 × 10 × 30 cm</p>
-        <p>Dimensions:20 × 10 × 30 cm</p>
-      </div>
+      {renderTabs()}
     </div>
   );
 };
