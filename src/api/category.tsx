b61109433c5f0ab1e71fa8e74e1df9934eac9601
liemchao <liemro9x@gmail.com>
2024-02-20 16:13:32 +0700

    error page

--- src/api/category.tsx
+++ src/api/category.tsx
@@ -3,7 +3,7 @@ import axiosInstance from "@/config/axios"
 const getCategoryAPI = async ()=>{
     try {
         const respone = await  axiosInstance.get("/category",
-        {headers:{Authorization:"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRpbnRyYW4iLCJzdWIiOiI2NWJhM2FhMDdhYTBhMGY0MWYwYTUzNWMiLCJpYXQiOjE3MDgzMTg1NzcsImV4cCI6MTcwODMxODg3N30.S2nOCm3iULg-VqIJVCqpVpg-hM9-dvSvtC3Z2j-720U"}}
+        {headers:{Authorization:""}}
         )
         return respone
     } catch (error) {
