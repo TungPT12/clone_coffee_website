d52c8da5f5583af3fe80a1f4a334a595bfd351e9
LiemSgod <liemro9x@gmail.com>
2024-03-11 18:20:33 +0700

    fix filter

--- src/types/entities/catgory.entity.ts
+++ src/types/entities/catgory.entity.ts
@@ -1,16 +1,18 @@
-type product = {
-  _id: string;
-  name: string;
-  images: string[];
-  price_original: number;
-  price_new: number;
-};
+// type product = {
+//   _id: string;
+//   name: string;
+//   images: string[];
+//   price_original: number;
+//   price_new: number;
+// };
+
+import { Product } from "./product.entity";
 
 export type StoreCatgory = {
   _id: string;
   image: string;
   createdAt: Date;
   updatedAt: Date;
-  products: product[];
+  products: Product[];
   name: string;
 };
